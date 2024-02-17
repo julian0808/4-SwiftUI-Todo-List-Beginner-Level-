37dd2f9c4c7f18c31a9d48904e9d0b79b7bccbf0
julian0808 <julianm08@gmail.com>
2024-02-17 09:40:36 -0500

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Julian Mesa on 17/02/24.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
